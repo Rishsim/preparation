68fcaf2ab920b6e32452dad7bcee10b83625e633
LAPTOP-EFSKR9KV\ramya <ramya0502@gmail.com>
2022-11-10 22:02:54 +0000

    10/11/2022

---
+++ Preparation/DataType/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType
+{
+    public class Class1
+    {
+
+        int i = 10;
+    }
+}
