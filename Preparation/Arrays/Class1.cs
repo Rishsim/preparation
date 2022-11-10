04d2687acf71505121c7e90016e95309ca349bd2
LAPTOP-EFSKR9KV\ramya <ramya0502@gmail.com>
2022-11-10 22:07:13 +0000

    arry10/11

---
+++ Preparation/Arrays/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class Rish
+    { 
+        int[] g = { 5, 6, 5 };
+        float[] h = { 10.5f, 10.7f, 10.6f };
+        double[] i = { 8577224d, 087662d, 444555d };
+        string[] j = { "raj","rag","ria" };
+        char[] k = {'s','t','u'};
+        bool[] l = { true, false, true };
+
+    }
+    public class Simmi
+    {
+        int[] m = { 7, 8, 9 };
+        string[] n = { "kia","kiaan","kaki"};
+
+
+    }
+
+
+}
